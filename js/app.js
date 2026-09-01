064739a55d83715fb623623a2510c1ae56c6f7a5
Nazarene <nazarenegena@users-MacBook-Pro.local>
2026-09-01 12:14:52 +0300

    update next question added

--- js/app.js
+++ js/app.js
@@ -1,39 +1,65 @@
 import { questions } from "./questions.js";
 
-const quizBox = document.getElementsByClassName("all-questions");
-const firstQuestion = quizBox[0];
+const quizBox = document.getElementById("all-questions");
 let currentQuestionIndex = 0;
+let scoreCount = 0;
+let isQuizOver = false;
 const answerContainer = document.querySelector(".answer-container")
 const scoreDisplay = document.getElementById("score");
 const wrongAns = document.createElement('div')
- const wrongAnsText = document.createElement('p')
+const wrongAnsText = document.createElement('p')
 const nextButton = document.createElement('button');
-const restartButton = document.createElement('button');
+nextButton.textContent = "Next Question"
 nextButton.addEventListener('click', handleNextButtonClick);
-restartButton.addEventListener('click', handleRestartButtonClick);
-let scoreCount = 0;
+nextButton.style.display = "none"
+ answerContainer.appendChild(nextButton)
 
+const endOfQuiz = document.getElementById("end-of-questions")
 
 function handleNextButtonClick() {
-  if (currentQuestionIndex < questions.length - 1) {
-    currentQuestionIndex ++
-    displayQuestion()
+  try {
+    if (currentQuestionIndex < questions.length - 1) {
+      currentQuestionIndex++
+      displayQuestion()
+  nextButton.style.display = "none"
+    }
+    else {
+      isQuizOver = true;
+      endOfQuiz.innerHTML = "";
+      quizBox.innerHTML = "";
+      const endQuizParagraph = document.createElement("p");
+      endQuizParagraph.textContent = "Yeey you have reached the end of Quizie Engine"
+      const restartButton = document.createElement('button');
+      restartButton.textContent = "Restart Quiz"
+      restartButton.addEventListener('click', handleRestartButtonClick);
+      endOfQuiz.appendChild(endQuizParagraph)
+      endOfQuiz.appendChild(restartButton)
+      nextButton.style.display = "none"
+    }
+  } catch (err)
+  {
+    console.log(err)
   }
-  else {
-    alert("You've reached the end of the quiz!")
-  }
-}
-function handleRestartButtonClick() {
-  currentQuestionIndex = 0;
-  scoreCount = 0;
-  scoreDisplay.textContent = "";
-  wrongAns.innerHTML=""
-  firstQuestion.innerHTML = "";
-  displayQuestion();
 }
 
+function handleRestartButtonClick() {
+questions.forEach(questionItem => {
+    currentQuestionIndex=0
+    questionItem.isAnswered = false
+    scoreCount = 0;
+    scoreDisplay.textContent = "";
+    wrongAns.innerHTML=""
+    quizBox.innerHTML=""
+  endOfQuiz.innerHTML = "";
+  nextButton.style.display = "none"
+  isQuizOver = false;
+
+  })
+displayQuestion();
+}
 
 function displayQuestion() {
+  quizBox.innerHTML=""
   const question = questions[currentQuestionIndex];
     const questionDiv = document.createElement('div')
     const questionCategory = document.createElement('p')
@@ -49,47 +75,40 @@ function displayQuestion() {
       btn.addEventListener("click", handleChoiceClick)
       choicesDiv.appendChild(btn)
     })
+
   function handleChoiceClick(e) {
+    if (isQuizOver) return;
     if (question.answer === e.target.textContent) {
-      if (question.answer === e.target.textContent) {
+
           if (!question.isAnswered) {
             scoreCount += 1;
-        question.isAnswered=true
+            question.isAnswered = true
           }
-         wrongAnsText.textContent = ""
-          scoreDisplay.textContent = `Great job! Your score is ${scoreCount}`;
-
-        }
-
-        nextButton.textContent = "Next Question"
-        restartButton.addEventListener(("click"), handleRestartButtonClick)
-        restartButton.textContent = "Restart Quiz"
- scoreDisplay.textContent = `Great job! Your score is ${scoreCount}`;
-        answerContainer.appendChild(scoreDisplay)
-        answerContainer.appendChild(nextButton)
-        answerContainer.appendChild(restartButton)
+      wrongAnsText.textContent = ""
+      scoreDisplay.textContent = `Your score is ${scoreCount}`;
+      nextButton.style.display = "block"
+          // answerContainer.appendChild(scoreDisplay)
 
       }
+    else {
 
-      else {
+      wrongAnsText.textContent = "Wrong Answer, Try again"
+      nextButton.style.display = "none"
+      scoreDisplay.textContent = "";
+      wrongAns.appendChild(wrongAnsText)
+      questionDiv.appendChild(wrongAns)
 
-        wrongAnsText.textContent = "Wrong Answer, Try again"
-        scoreDisplay.textContent = "";
-        wrongAns.appendChild(wrongAnsText)
       }
-      console.log(scoreCount)
-    }
-
+  }
     questionCategory.textContent = question.category;
     questionDifficulty.textContent = question.difficulty;
     questionText.textContent = question.question;
-    firstQuestion.appendChild(questionDiv);
+    quizBox.appendChild(questionDiv);
      questionDiv.setAttribute("id", question.id)
     questionDiv.appendChild(questionCategory);
     questionDiv.appendChild(questionDifficulty);
     questionDiv.appendChild(questionText);
   questionDiv.appendChild(choicesDiv);
-  questionDiv.appendChild(wrongAns)
-}
 
+}
   displayQuestion()
